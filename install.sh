e1facd69b310e758397fe76f93552575341ce0c6
zzfhao <zzfhao1993@gmail.com>
2019-03-14 17:06:30 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git remote add staging https://github.com/Telecominfraproject/oopt-gnpy.git
+
+git remote
+
+git fetch staging
+
+git branch -r
+
+git checkout -b staging staging/phase-1
+
+sudo apt-get install python3-pip
+
+pip3 install numpy
+
+pip3 install scipy
+
+pip3 install matplotlib
+
+sudo apt-get install python3-tk
